799dde8d92993a69cafda382102ae36d6ebbd398
omkark1558-byte <omkark1558@gmail.com>
2026-06-04 00:31:36 +0530

    Repo Appointment

---
+++ Aopllo_Hospital/src/main/java/com/ecom/Aopllo_Hospital/repository/Appointment_Repo.java
@@ -0,0 +1,8 @@
+package com.ecom.Aopllo_Hospital.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ecom.Aopllo_Hospital.entity.Appointment;
+
+public interface Appointment_Repo extends JpaRepository<Appointment, Integer> {
+
+}
