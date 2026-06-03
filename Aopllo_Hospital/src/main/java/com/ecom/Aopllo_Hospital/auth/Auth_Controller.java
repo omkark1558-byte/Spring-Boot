bc0ab4ca87a7c726c4e1f51836ae92638f5628a1
omkark1558-byte <omkark1558@gmail.com>
2026-06-03 20:21:53 +0530

    Auth Modified

--- Aopllo_Hospital/src/main/java/com/ecom/Aopllo_Hospital/auth/Auth_Controller.java
+++ Aopllo_Hospital/src/main/java/com/ecom/Aopllo_Hospital/auth/Auth_Controller.java
@@ -2,14 +2,17 @@ package com.ecom.Aopllo_Hospital.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecom.Aopllo_Hospital.Dto.Login_DTO;
+import com.ecom.Aopllo_Hospital.Dto.Register_DTO;
+import com.ecom.Aopllo_Hospital.entity.User_entity;
+import com.ecom.Aopllo_Hospital.repository.User_Repo;
 import com.ecom.Aopllo_Hospital.security.JwtUtil;
 import org.springframework.web.bind.annotation.RequestBody;
- 
 
 @RestController
 @RequestMapping("/auth")
@@ -17,6 +20,12 @@ public class Auth_Controller {
 
 	@Autowired
 	private JwtUtil jwtUtil;
+
+	@Autowired
+	private User_Repo userRepo;
+
+	@Autowired
+	private PasswordEncoder encoder;
 
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody Login_DTO dto) {
@@ -29,6 +38,19 @@ public class Auth_Controller {
 		}
 
 		return ResponseEntity.badRequest().body("Invalid Credentials");
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity<?> register(@RequestBody Register_DTO dto) {
+		User_entity user = new User_entity();
+
+		user.setUsername(dto.getUsername());
+		user.setPassword(encoder.encode(dto.getPassword()));
+		user.setRole(dto.getRole());
+
+		userRepo.save(user);
+
+		return ResponseEntity.ok("User Registered Successfully");
 	}
 
 }
