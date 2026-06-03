31e362d40b51584f041765aa36701482e5888e99
omkark1558-byte <omkark1558@gmail.com>
2026-06-03 20:26:50 +0530

    Status DTO

---
+++ Aopllo_Hospital/src/main/java/com/ecom/Aopllo_Hospital/Dto/Status_DTO.java
@@ -0,0 +1,14 @@
+package com.ecom.Aopllo_Hospital.Dto;
+
+public class Status_DTO {
+
+	private String status;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
